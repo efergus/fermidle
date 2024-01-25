fbcab62f1693b341c4d9ca93ee06801535ce6f17
Ethan Ferguson <sometimescode@gmail.com>
2024-01-25 17:53:59 -0500

    Add intro page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full h-screen flex flex-col justify-center items-center gap-4">
+	<h1>Hello Adam</h1>
+	<p class="border-b border-black">Visit <a class="hover:border-b-2 hover:bg-slate-100 px-1 -mx-0.5 border-black" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -22,6 +22,7 @@ const config = {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
