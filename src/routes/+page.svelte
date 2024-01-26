0a7319bdc844d0708256bef3dbd026fe7a1fdb8b
Ethan Ferguson <sometimescode@gmail.com>
2024-01-26 16:50:26 -0500

    Works in both dark and light mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,19 @@
-<div class="w-full h-screen flex flex-col justify-center items-center gap-4">
-	<h1>Hello Adam</h1>
-	<p class="border-b border-black">Visit <a class="hover:border-b-2 hover:bg-slate-100 px-1 -mx-0.5 border-black" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import DarkModeButton from './DarkModeButton.svelte';
+</script>
+
+<div class="w-full h-screen vrt justify-stretch bg-theme">
+	<div class="w-full hrz justify-end"><DarkModeButton /></div>
+	<div class="vrt center gap-2 grow">
+		<h1>Hello Adam</h1>
+		<p class="border-b">
+			Visit <a class="hover:bg-secondary px-1 -mx-0.5" href="https://kit.svelte.dev"
+				>kit.svelte.dev</a
+			> to read the documentation
+		</p>
+		<div class="p-4 rounded-lg vrt items-stretch gap-2">
+			<p class="p-2 rounded-lg bg-primary grow text-center">Primary Background</p>
+			<p class="py-2 px-4 rounded-lg bg-secondary">Secondary Background</p>
+		</div>
+	</div>
 </div>
