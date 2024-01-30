ce55162f0eba890f2e1b65d6516ecff12e87b36e
Adam Wilson <wilso@adams-mbp.mynetworksettings.com>
2024-01-29 19:46:16 -0500

    text box working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,21 @@
 <script>
 	import DarkModeButton from './DarkModeButton.svelte';
+	import Guesser from './Guesser.svelte';
 </script>
 
 <div class="w-full h-screen vrt justify-stretch bg-theme">
 	<div class="w-full hrz justify-end"><DarkModeButton /></div>
 	<div class="vrt center gap-2 grow">
-		<h1>Hello Adam</h1>
-		<p class="border-b">
+		<!-- <h1>Hello Adam</h1> -->
+		<!-- <p class="border-b">
 			Visit <a class="hover:bg-secondary px-1 -mx-0.5" href="https://kit.svelte.dev"
 				>kit.svelte.dev</a
 			> to read the documentation
-		</p>
-		<div class="p-4 rounded-lg vrt items-stretch gap-2">
+		</p> -->
+		<!-- <div class="p-4 rounded-lg vrt items-stretch gap-2">
 			<p class="p-2 rounded-lg bg-primary grow text-center">Primary Background</p>
 			<p class="py-2 px-4 rounded-lg bg-secondary">Secondary Background</p>
-		</div>
+		</div> -->
+		<div class="center"><Guesser /></div>
 	</div>
 </div>
---
+++ src/routes/Guesser.svelte
@@ -0,0 +1,39 @@
+<script>
+    import '../app.css';
+    let base = 5; //TODO: get actual base from data etc
+
+    let inputField;
+    let newFieldValue = '';
+
+    function isNumeric(str) {
+        if (typeof str != "string") return false // we only process strings!  
+        return !isNaN(parseInt(str)) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+                !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+     }
+
+    const onInput = (event) => {
+        if (event.key !== 'Enter') return;
+        if (!isNumeric(newFieldValue)) return;
+        console.log(newFieldValue);
+        inputField.value = '';
+    };
+</script>
+
+<div class="hrz items-start">
+    <b class="text-5xl">
+        {base} ⋅ 10
+    </b>
+    <input
+        class="border-2 rounded border-contrast max-w-32"
+        name="inputField1"
+        type="text"
+        placeholder="Guess..."
+        bind:this={inputField}
+        on:keydown={onInput}
+        bind:value={newFieldValue} />
+</div>
+<!-- <p>User input: {newFieldValue || "..."}</p> -->
+
+<style>
+
+</style>
