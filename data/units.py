9aba31f87fc7cfb552c4166bf04dba89880f6f89
Ethan Ferguson <sometimescode@gmail.com>
2024-02-17 21:51:34 -0500

    Unit standardization working (no units with offsets)

--- data/units.py
+++ data/units.py
@@ -5,6 +5,9 @@ from dataclasses import InitVar, dataclass, field, asdict
 import re
 from math import prod, log10, floor
 from copy import copy, deepcopy
+from collections import defaultdict
+
+number_regex = r"-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?"
 
 prefixes = {
     "T": 12,
@@ -19,14 +22,20 @@ prefixes = {
     "p": -12,
 }
 
+prefixes_by_magnitude = {
+    val: key for key, val in prefixes.items()
+}
+
 @dataclass
 class Conversion:
-    to_unit: InitVar[str | Unit | List[Unit] | Units]
-    to: Unit = field(init=False)
+    # to_unit: InitVar[str | Unit | List[Unit] | Units]
+    to: Unit
     ratio: float = 1
     offset: float = 0
+    magnitude: int = 0
 
-    def __post_init__(self, to_unit):
+    def __post_init__(self):
+        to_unit = self.to
         t = type(to_unit)
         if t == str:
             self.to = Units([Unit(to_unit)])
@@ -44,11 +53,23 @@ class Conversion:
         offset = self.offset*self.ratio + conversion.offset
         return Conversion(units, ratio, offset)
     
-    def standardized(self, map: ConversionMap):
-        units = [unit.standardized(map) for unit in self.to]
-
-
-ConversionMap = Dict[str, Conversion | None]
+    def standardized(self, map: ConversionMap) -> Conversion:
+        units = []
+        ratio = self.ratio
+        # TODO: handle this at all lol
+        offset = self.offset
+        for unit in self.to:
+            name = unit.name
+            if name not in map or map[name] == None:
+                units.append(copy(unit))
+                continue
+            conversion = map[name].standardized(map)
+            ratio *= conversion.ratio
+            to = [copy(unit) for unit in conversion.to]
+            for child in to:
+                child.power *= unit.power
+            units.extend(conversion.to)
+        return Conversion(units, ratio, offset)
 
 @dataclass
 class Unit:
@@ -62,7 +83,7 @@ class Unit:
         if not m:
             raise ValueError(f"Could not parse unit '{string}'")
         name, power = m.group(1, 2)
-        power = int(power) if power else 0
+        power = int(power) if power else 1
         magnitude = 0
         while name not in known:
             if name[-1] == 's' and name[:-1] in known:
@@ -76,7 +97,16 @@ class Unit:
         return Unit(name, power, magnitude)
     
     def __str__(self):
-        return f"{self.name}{self.power}"
+        return f"{prefixes_by_magnitude.get(self.magnitude, '')}{self.name}{self.power}"
+    
+    def __pow__(self, power):
+        return Unit(self.name, self.power * power, self.magnitude)
+    
+    def powerless(self):
+        return Unit(self.name, 1, self.magnitude)
+    
+    def magnitudeless(self):
+        return Unit(self.name, self.power)
     
     # def preferred(self, known: Dict[str, Conversion]):
     #     if self.name not in known:
@@ -88,9 +118,13 @@ class Unit:
     #     # for unit in conversion.to:
     #     return conversion.ratio, conversion.to
 
-# def scientific(num: float, precision: int=None):
-#     magnitude = floor(log10(num))
-    
+def scientific(num: float, precision: int=None):
+    magnitude = floor(log10(num))
+    if precision != None:
+        num *= 10**(precision - magnitude - 1)
+        num = floor(num)/10**(precision - 1)
+        return f"{num}e{magnitude}"
+    return f"{num / 10**magnitude}e{magnitude}"
 
 def combine_units(preferred_units):
     ratios = [ratio for ratio, _, _ in preferred_units]
@@ -107,7 +141,7 @@ class Units:
         string = string.replace("^", "")
         positive, _, negative = string.partition("/")
         def helper(string: str):
-            unit_strings = re.findall(r'([^\d/]+\d*)', string)
+            unit_strings = re.findall(r'([^\d/*]+\d*)', string)
             return [Unit._parse(s, known) for s in unit_strings]
         positive = helper(positive)
         negative = helper(negative)
@@ -135,6 +169,29 @@ class Units:
     
     def standardized(self, known):
         return Units._standardized(self, known)
+    
+    def mul(self, units: Units):
+        units = Units(self.units + units.units)
+        units.simplify()
+        return units
+    
+    def simplify(self):
+        units = defaultdict(int)
+        for unit in self.units:
+            units[(unit.name, unit.magnitude)] += unit.power
+        self.units = [Unit(name, power, magnitude) for (name, magnitude), power in units.items() if power != 0]
+
+    def sort(self):
+        self.units.sort(key=lambda x: (x.power, x.name, x.magnitude))
+
+    def __len__(self):
+        return len(self.units)
+
+    def __mul__(self, other):
+        return self.mul(other)
+    
+    def __pow__(self, power):
+        return Units([unit ** power for unit in self.units])
 
     def __str__(self):
         units = sorted(self.units, key=lambda x: -x.power)
@@ -159,32 +216,72 @@ class Units:
     
     def __iter__(self):
         return self.units.__iter__()
-    
+
 @dataclass
 class Quantity:
     value: float
-    units: Units
+    units: Units = field(default_factory=lambda: Units([]))
     uncertainty: float = 0
-
-    def standardized(self, map: ConversionMap):
-        conversion = self.units.standardized(map)
-        value = (self.value + conversion.offset) * conversion.ratio * 10**conversion.magnitude
-        return Quantity(value, conversion.to, self.uncertainty*conversion.ratio)
 
     @staticmethod
     def from_str(string: str):
-        number, _, units = string.partition(' ')
-        units = Units.parse(units, known)
-        return Quantity(float(number), units)
+        rest = r"\s*(.*)"
+        match = re.match(f"({number_regex})?{rest}", string)
+        if not match:
+            print(f"Could not parse quantity '{string}'")
+        number, units = match.group(1, 2)
+        units = Units.parse(units, preferred)
+        return Quantity(float(number or '1'), units)
 
-def canonicalize_conversion(conversion) -> Conversion:
-    if type(conversion) == str:
-        return Conversion(conversion)
-    if type(conversion) == Unit:
-        return Conversion([conversion])
-    return conversion
+    def standardized(self, map: ConversionMap):
+        res = deepcopy(self)
+        for i in range(0, 100):
+            simplifier = Quantity(1)
+            for unit in res.units:
+                name = unit.name
+                if unit.magnitude:
+                    simplifier *= Quantity(10**(unit.magnitude*unit.power), Units([unit.magnitudeless(), unit**-1]))
+                elif name in map and map[name]:
+                    simplifier *= map[name]**unit.power
+            if len(simplifier.units):
+                res *= simplifier
+            else:
+                break
+        if i == 0:
+            raise 
+        return res
+    
+    def mul(self, other: Quantity):
+        value = self.value
+        units = self.units
+        value = value * other.value
+        units = units * other.units
+        return Quantity(value, units)
 
-known = {
+    def __mul__(self, other):
+        t = type(other)
+        if t == Quantity:
+            return self.mul(other)
+        if t == Unit:
+            return self.mul(Quantity(1, Units([other])))
+        
+    def __pow__(self, power):
+        return Quantity(self.value ** power, self.units ** power)
+        
+    def __str__(self):
+        return f"{scientific(self.value, 3)} {self.units}"
+        
+def canonicalize_quantity_map(unit, quantity) -> Quantity:
+    if not quantity:
+        return
+    if type(quantity) == str:
+        return Quantity.from_str(quantity) * Unit(unit) ** -1
+    return quantity * Unit(unit) ** -1
+    
+
+ConversionMap = Dict[str, Quantity | None]
+
+preferred = {
     "g": None,
     "m": None,
     "s": None,
@@ -192,40 +289,79 @@ known = {
     "pH": None,
     "Hz": None,
     "J": None,
-    # Technically the unit is Bells
-    "dB": None,
     # imperial
-    "in": Conversion("m", 2.54),
-    "ft": Conversion("m", 0.3048),
-    "yard": Conversion("m", 0.9144),
-    "oz": Conversion("g", 28.3495),
-    "lb": Conversion("g", 453.5924),
-    "mph": Conversion("kph", 1.60934),
-    "mile": Conversion("km", 1.60934),
+    "in": "2.54e-2 m",
+    "ft": "0.3048 m",
+    "yd": "0.9144 m",
+    "yard": "yd",
+    "oz": "28.3495 g",
+    "lb": "453.5924 g",
+    "mph": "mile/h",
+    "mile": "1.60934 km",
     # standard, unusual units
-    "ton": Unit("g", magnitude=6),
-    "picometer": "pm",
-    "kph": Conversion([Unit("m"), Unit("s", -1)], 5/18),
-    "eV": Conversion("J", 1.60218e-19),
-    "C": Conversion("K", 1, 273.15),
+    # "ton": Unit("g", magnitude=6),
+    # "picometer": "pm",
+    # "kph": Conversion([Unit("m"), Unit("s", -1)], 5/18),
+    # "eV": Conversion("J", 1.60218e-19),
+    # "C": Conversion("K", 1, 273.15),
+    "ly": "9.461e15 m",
 
     "h": "hour",
-    "hour": Conversion("s", 3600),
-    "year": Conversion("s", 365*24*60*60),
+    "hour": "3600 s",
+    "yr": "year",
+    "year": Quantity(365*24*60*60, Units([Unit("s")])),
 }
 
-known = {unit: canonicalize_conversion(conversion) for unit, conversion in known.items()}
+preferred = {unit: canonicalize_quantity_map(unit, quantity) for unit, quantity in preferred.items()}
 
-def test(units: str):
-    print("Convert:", units)
-    value = Quantity.from_str(units)
-    # print(value)
-    value.standardized(known)
+# known = {
+#     "g": None,
+#     "m": None,
+#     "s": None,
+#     "bit": None,
+#     "pH": None,
+#     "Hz": None,
+#     "J": None,
+#     # Technically the unit is Bells
+#     "dB": None,
+#     # imperial
+#     "in": Conversion("m", 2.54e-2),
+#     "ft": Conversion("m", 0.3048),
+#     "yard": Conversion("m", 0.9144),
+#     "oz": Conversion("g", 28.3495),
+#     "lb": Conversion("g", 453.5924),
+#     "mph": Conversion("kph", 1.60934),
+#     "mile": Conversion("km", 1.60934),
+#     # standard, unusual units
+#     "ton": Unit("g", magnitude=6),
+#     "picometer": "pm",
+#     "kph": Conversion([Unit("m"), Unit("s", -1)], 5/18),
+#     "eV": Conversion("J", 1.60218e-19),
+#     "C": Conversion("K", 1, 273.15),
+
+#     "h": "hour",
+#     "hour": Conversion("s", 3600),
+#     "year": Conversion("s", 365*24*60*60),
+# }
+
+# known = {unit: canonicalize_quantity_map(unit, quantity) for unit, quantity in known.items()}
+
+def test(value1: str, value2: str):
+    quant1 = Quantity.from_str(value1)
+    quant2 = Quantity.from_str(value2)
+    quant3 = quant1 * quant2
+    print(quant3)
+    standardized = quant3.standardized(preferred)
+    print(standardized)
 
 if __name__ == "__main__":
-    test("1 km^2/h")
-    test("1 ft^2/h")
-    test("1 kft^2/h")
-    test("1 s/h")
-    test("1 mph/s")
-    test("1 miles")
+    test("1 km^2/s", "0.5 ly/yr")
+    test("1 ft^2/h", "2")
+    # test("1 kft^2/h")
+    # test("1 s/h")
+    # test("1 mph/s")
+    # test("1 miles")
+    # for unit, conversion in known.items():
+    #     if conversion:
+    #         print("Unit:", unit)
+    #         print(conversion.standardized(known))
